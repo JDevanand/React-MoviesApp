065351be2052c6b47b0cde0abf8152d3048f90d7
JDevanand <devanand.jayakumar@gmail.com>
2021-05-23 16:24:30 +0530

    Added 'BookShow' component and Book Show button on header on details page

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import ReactDom from 'react-dom';
 import './Header.css';
+import BookShow from '../../screens/bookshow/BookShow';
 import Button from '@material-ui/core/Button';
 import logo from '../../assets/logo.svg';
 import Modal from 'react-modal';
@@ -27,7 +29,7 @@ const customStyles = {
 // here textAlign is used to center the login form and not just the texts in it
 const TabContainer = function (props) {
     return (
-        <Typography component="div" style={{ padding: 0, textAlign:'center'}}> 
+        <Typography component="div" style={{ padding: 0, textAlign: 'center' }}>
             {props.children}
         </Typography>
     );
@@ -36,8 +38,8 @@ const TabContainer = function (props) {
 //Below is required to make sure that the "Tabcontainer has childrens"
 //This is called type checking 
 //Code will stil run but throws console error if there are no children to tabContainer
-TabContainer.propTypes={
-    children:PropTypes.node.isRequired
+TabContainer.propTypes = {
+    children: PropTypes.node.isRequired
 }
 
 class Header extends Component {
@@ -47,24 +49,24 @@ class Header extends Component {
         this.state = {
             modalIsOpen: false, //required for modal
             value: 0,             //required for Tabs
-            
-            username:"",                  // state variable to capture the input field of username
-            usernameRequired:"dispNone",  //dispNone is className with css defined as none. 
 
-            password:"",
-            passwordRequired:"dispNone",
+            username: "",                  // state variable to capture the input field of username
+            usernameRequired: "dispNone",  //dispNone is className with css defined as none. 
+
+            password: "",
+            passwordRequired: "dispNone",
 
             register: {
-                firstname:"",
-                firstnameRequired:"dispNone",
-                lastname:"",
-                lastnameRequired:"dispNone",
-                email:"",
-                emailRequired:"dispNone",
-                password:"",
-                passwordRequired:"dispNone",
-                contactno:"",
-                contactnoRequired:"dispNone"
+                firstname: "",
+                firstnameRequired: "dispNone",
+                lastname: "",
+                lastnameRequired: "dispNone",
+                email: "",
+                emailRequired: "dispNone",
+                password: "",
+                passwordRequired: "dispNone",
+                contactno: "",
+                contactnoRequired: "dispNone"
             }
         };
     }
@@ -73,52 +75,57 @@ class Header extends Component {
         this.setState({ modalIsOpen: true });
     }
 
-    closeModalHandler = () => {        
-        this.setState({ modalIsOpen: false, 
-                        usernameRequired:'dispNone', //usename and password "required" helpertext needs to be set to displaynone on closing
-                        passwordRequired:'dispNone', //this is to avoid the side-effect of the "required" helpertext been dispalyed when closing & opening modal again
-                        value:0,    //this is required to make the "login" tab while opening modal again, in case the "register" tab was selected
-                        register:{
-                            firstnameRequired:"dispNone",
-                            lastnameRequired:"dispNone",
-                            emailRequired:"dispNone",
-                            passwordRequired:"dispNone",
-                            contactnoRequired:"dispNone"
-                        } 
-                    });   
+    closeModalHandler = () => {
+        this.setState({
+            modalIsOpen: false,
+            usernameRequired: 'dispNone', //usename and password "required" helpertext needs to be set to displaynone on closing
+            passwordRequired: 'dispNone', //this is to avoid the side-effect of the "required" helpertext been dispalyed when closing & opening modal again
+            value: 0,    //this is required to make the "login" tab while opening modal again, in case the "register" tab was selected
+            register: {
+                firstnameRequired: "dispNone",
+                lastnameRequired: "dispNone",
+                emailRequired: "dispNone",
+                passwordRequired: "dispNone",
+                contactnoRequired: "dispNone"
+            }
+        });
     }
 
     tabChangeHandler = (event, value) => {
         this.setState({ value });
     }
 
-    loginClickHandler=()=>{
-        this.state.username ==="" ? this.setState({usernameRequired:'dispBlock'}) : this.setState({usernameRequired:'dispNone'});
-        this.state.password ==="" ? this.setState({passwordRequired:'dispBlock'}) : this.setState({passwordRequired:'dispNone'});
+    loginClickHandler = () => {
+        this.state.username === "" ? this.setState({ usernameRequired: 'dispBlock' }) : this.setState({ usernameRequired: 'dispNone' });
+        this.state.password === "" ? this.setState({ passwordRequired: 'dispBlock' }) : this.setState({ passwordRequired: 'dispNone' });
     }
 
-    onInputUsernameChange=(e)=>{
-        this.setState({username:e.target.value});
+    onInputUsernameChange = (e) => {
+        this.setState({ username: e.target.value });
     }
 
-    onInputPasswordChange=(e)=>{
-        this.setState({password:e.target.value});
+    onInputPasswordChange = (e) => {
+        this.setState({ password: e.target.value });
     }
 
-    onRegisterFormInputChange=(e)=>{
+    onRegisterFormInputChange = (e) => {
         const state = this.state;
         state.register[e.target.name] = e.target.value;
-        this.setState({state:state});
+        this.setState({ state: state });
     }
 
-    registerClickHandler=()=>{
+    registerClickHandler = () => {
         const state = this.state.register;
-        state.firstname==="" ? state.firstnameRequired="dispBlock" : state.firstnameRequired='dispNone';
-        state.lastname==="" ? state.lastnameRequired="dispBlock" : state.lastnameRequired='dispNone';
-        state.email==="" ? state.emailRequired="dispBlock" : state.emailRequired='dispNone';
-        state.password==="" ? state.passwordRequired="dispBlock" : state.passwordRequired='dispNone';
-        state.contactno==="" ? state.contactnoRequired="dispBlock" : state.contactnoRequired='dispNone';        
-        this.setState({state:state});
+        state.firstname === "" ? state.firstnameRequired = "dispBlock" : state.firstnameRequired = 'dispNone';
+        state.lastname === "" ? state.lastnameRequired = "dispBlock" : state.lastnameRequired = 'dispNone';
+        state.email === "" ? state.emailRequired = "dispBlock" : state.emailRequired = 'dispNone';
+        state.password === "" ? state.passwordRequired = "dispBlock" : state.passwordRequired = 'dispNone';
+        state.contactno === "" ? state.contactnoRequired = "dispBlock" : state.contactnoRequired = 'dispNone';
+        this.setState({ state: state });
+    }
+
+    bookshowClickHandler = (e) => {
+        ReactDom.render(<BookShow />,document.getElementById('root'));
     }
 
     render() {
@@ -126,11 +133,18 @@ class Header extends Component {
             <div>
                 <header className="app-header">
                     <img src={logo} className="app-logo" alt="logo" />
-                    <div className="login-button">
-                        <Button variant="contained" color="default" onClick={this.openModalHandler}>
-                            Login
-                        </Button>
-                    </div>
+
+                        <div className="login-button">
+                            <Button variant="contained" color="default" onClick={this.openModalHandler}>
+                                Login
+                            </Button>
+                        </div>                  
+                        {this.props.showBookShowButton==='true' ?
+                        <div className="bookshow-button">
+                            <Button variant="contained" color="primary" onClick={this.bookshowClickHandler}>
+                                Book Show
+                            </Button>
+                        </div> : "" }
                 </header>
 
                 <Modal
@@ -145,76 +159,76 @@ class Header extends Component {
                         <Tab label="Register"></Tab>
                     </Tabs>
 
-                    
-                    {
-                    //This check is to make sure that login shows only for login tab                      
-                    this.state.value ===0 &&
-                    <TabContainer>
-                        <FormControl required>
-                            <InputLabel htmlFor="Username">Username:</InputLabel>
-                            <Input id="Username" type="text" username={this.state.username} onChange={this.onInputUsernameChange}></Input>
-                            <FormHelperText className={this.state.usernameRequired}>
-                                <span className="red">required</span>
-                            </FormHelperText>
-                        </FormControl><br/><br/>
-                        <FormControl required>
-                            <InputLabel htmlFor="Password" >Password:</InputLabel>
-                            <Input id="Password" type="password" password={this.state.password} onChange={this.onInputPasswordChange}></Input>
-                            <FormHelperText className={this.state.passwordRequired}>
-                                <span className="red">required</span>
-                            </FormHelperText>
-                        </FormControl><br/><br/>
-                        <Button variant="contained" color="primary" onClick={this.loginClickHandler}>LOGIN</Button>
-                    </TabContainer>
-                    }   
 
                     {
-                    //This check is to make sure that login shows only for register tab                      
-                    this.state.value ===1 &&
-                    <TabContainer>
-                        <FormControl required>
-                            <InputLabel htmlFor="Firstname">Firstname:</InputLabel>
-                            <Input name="firstname" id="Firstname" type="text" firstname={this.state.register.firstname} onChange={this.onRegisterFormInputChange}></Input>
-                            <FormHelperText className={this.state.register.firstnameRequired}>
-                                <span className="red">required</span>
-                            </FormHelperText>
-                        </FormControl><br/><br/>
+                        //This check is to make sure that login shows only for login tab                      
+                        this.state.value === 0 &&
+                        <TabContainer>
+                            <FormControl required>
+                                <InputLabel htmlFor="Username">Username:</InputLabel>
+                                <Input id="Username" type="text" username={this.state.username} onChange={this.onInputUsernameChange}></Input>
+                                <FormHelperText className={this.state.usernameRequired}>
+                                    <span className="red">required</span>
+                                </FormHelperText>
+                            </FormControl><br /><br />
+                            <FormControl required>
+                                <InputLabel htmlFor="Password" >Password:</InputLabel>
+                                <Input id="Password" type="password" password={this.state.password} onChange={this.onInputPasswordChange}></Input>
+                                <FormHelperText className={this.state.passwordRequired}>
+                                    <span className="red">required</span>
+                                </FormHelperText>
+                            </FormControl><br /><br />
+                            <Button variant="contained" color="primary" onClick={this.loginClickHandler}>LOGIN</Button>
+                        </TabContainer>
+                    }
 
-                        <FormControl required>
-                            <InputLabel htmlFor="Lastname">Lastname:</InputLabel>
-                            <Input name="lastname" id="Lastname" type="text" lastname={this.state.register.lastname} onChange={this.onRegisterFormInputChange}></Input>
-                            <FormHelperText className={this.state.register.lastnameRequired}>
-                                <span className="red">required</span>
-                            </FormHelperText>
-                        </FormControl><br/><br/>    
+                    {
+                        //This check is to make sure that login shows only for register tab                      
+                        this.state.value === 1 &&
+                        <TabContainer>
+                            <FormControl required>
+                                <InputLabel htmlFor="Firstname">Firstname:</InputLabel>
+                                <Input name="firstname" id="Firstname" type="text" firstname={this.state.register.firstname} onChange={this.onRegisterFormInputChange}></Input>
+                                <FormHelperText className={this.state.register.firstnameRequired}>
+                                    <span className="red">required</span>
+                                </FormHelperText>
+                            </FormControl><br /><br />
 
-                        <FormControl required>
-                            <InputLabel htmlFor="Email">Email:</InputLabel>
-                            <Input name="email" id="Email" type="text" email={this.state.register.email} onChange={this.onRegisterFormInputChange}></Input>
-                            <FormHelperText className={this.state.register.emailRequired}>
-                                <span className="red">required</span>
-                            </FormHelperText>
-                        </FormControl><br/><br/>   
+                            <FormControl required>
+                                <InputLabel htmlFor="Lastname">Lastname:</InputLabel>
+                                <Input name="lastname" id="Lastname" type="text" lastname={this.state.register.lastname} onChange={this.onRegisterFormInputChange}></Input>
+                                <FormHelperText className={this.state.register.lastnameRequired}>
+                                    <span className="red">required</span>
+                                </FormHelperText>
+                            </FormControl><br /><br />
 
-                        <FormControl required>
-                            <InputLabel htmlFor="Password">Password:</InputLabel>
-                            <Input name="password" id="Password" type="password" password={this.state.register.password} onChange={this.onRegisterFormInputChange}></Input>
-                            <FormHelperText className={this.state.register.passwordRequired}>
-                                <span className="red">required</span>
-                            </FormHelperText>
-                        </FormControl><br/><br/>
+                            <FormControl required>
+                                <InputLabel htmlFor="Email">Email:</InputLabel>
+                                <Input name="email" id="Email" type="text" email={this.state.register.email} onChange={this.onRegisterFormInputChange}></Input>
+                                <FormHelperText className={this.state.register.emailRequired}>
+                                    <span className="red">required</span>
+                                </FormHelperText>
+                            </FormControl><br /><br />
 
-                        <FormControl required>
-                            <InputLabel htmlFor="ContactNo">Contact No.:</InputLabel>
-                            <Input name="contactNo" id="ContactNo" type="text" contactno={this.state.register.contactno} onChange={this.onRegisterFormInputChange}></Input>
-                            <FormHelperText className={this.state.register.contactnoRequired}>
-                                <span className="red">required</span>
-                            </FormHelperText>
-                        </FormControl><br/><br/> 
+                            <FormControl required>
+                                <InputLabel htmlFor="Password">Password:</InputLabel>
+                                <Input name="password" id="Password" type="password" password={this.state.register.password} onChange={this.onRegisterFormInputChange}></Input>
+                                <FormHelperText className={this.state.register.passwordRequired}>
+                                    <span className="red">required</span>
+                                </FormHelperText>
+                            </FormControl><br /><br />
 
-                        <Button variant="contained" color="primary" onClick={this.registerClickHandler}>Register</Button>
-                    </TabContainer>
-                    }   
+                            <FormControl required>
+                                <InputLabel htmlFor="ContactNo">Contact No.:</InputLabel>
+                                <Input name="contactNo" id="ContactNo" type="text" contactno={this.state.register.contactno} onChange={this.onRegisterFormInputChange}></Input>
+                                <FormHelperText className={this.state.register.contactnoRequired}>
+                                    <span className="red">required</span>
+                                </FormHelperText>
+                            </FormControl><br /><br />
+
+                            <Button variant="contained" color="primary" onClick={this.registerClickHandler}>Register</Button>
+                        </TabContainer>
+                    }
                 </Modal>
             </div>
         )
---
+++ src/screens/bookshow/BookShow.js
@@ -0,0 +1,18 @@
+import React,{Component} from 'react';
+import ReactDom from 'react-dom';
+import './BookShow.css';
+import Header from '../../common/header/Header';
+
+class BookShow extends Component{
+    render(){
+        return(
+            <div>
+                <Header />
+            </div>
+        );
+    }
+}
+
+
+
+export default BookShow;
